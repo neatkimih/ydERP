21e540e10576ae682c38053ad40c550c0a18d925
User <User@YD02-05>
2018-04-03 11:23:24 +0900

    testcom

--- ydERP/src/main/java/com/yedam/erp/HomeController.java
+++ ydERP/src/main/java/com/yedam/erp/HomeController.java
@@ -31,7 +31,7 @@ public class HomeController {
 		
 		String formattedDate = dateFormat.format(date);
 		
-		model.addAttribute("SErverTime", formattedDate );
+		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
 	}

a13a212435e9144f6f1b69896ff2d4f8a9d51b02
User <User@YD02-09>
2018-04-03 17:36:36 +0900

    1

--- ydERP/src/main/java/com/yedam/erp/impl/PurchasesDAO.java
+++ ydERP/src/main/java/com/yedam/erp/impl/PurchasesDAO.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.yedam.erp.PurchasesVO;
+import com.yedam.erp.purchases.PurchasesVO;
 
 @Repository
 public class PurchasesDAO {

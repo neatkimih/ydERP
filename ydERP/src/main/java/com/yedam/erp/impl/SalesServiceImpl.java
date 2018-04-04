42588cfabc8625d248b24d9d1d5c601ae96e4eba
User <User@YD02-09>
2018-04-04 12:47:37 +0900

    1111

--- ydERP/src/main/java/com/yedam/erp/impl/SalesServiceImpl.java
+++ ydERP/src/main/java/com/yedam/erp/impl/SalesServiceImpl.java
@@ -1,7 +1,5 @@
 package com.yedam.erp.impl;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,15 +15,5 @@ public class SalesServiceImpl implements SalesService
 	@Override
 	public SalesVO selectOne(SalesVO vo) {
 		return salesDAO.selectOne(vo);
-	}
-
-	@Override
-	public void insert(SalesVO vo) {
-		salesDAO.insert(vo);
-	}
-
-	@Override
-	public void update(SalesVO vo) {
-		salesDAO.update(vo);
 	}
 }

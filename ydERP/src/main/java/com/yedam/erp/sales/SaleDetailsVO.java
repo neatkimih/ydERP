d20f68d080cbc2d53eded348a3447df28f043465
User <User@YD02-09>
2018-04-04 17:52:01 +0900

    z

---
+++ ydERP/src/main/java/com/yedam/erp/sales/SaleDetailsVO.java
@@ -0,0 +1,33 @@
+package com.yedam.erp.sales;
+
+public class SaleDetailsVO {
+	private String saleDetailCode;
+	private String saleCode;
+	private String saleItem;
+	private String saleQty;
+	
+	public String getSaleDetailCode() {
+		return saleDetailCode;
+	}
+	public void setSaleDetailCode(String saleDetailCode) {
+		this.saleDetailCode = saleDetailCode;
+	}
+	public String getSaleCode() {
+		return saleCode;
+	}
+	public void setSaleCode(String saleCode) {
+		this.saleCode = saleCode;
+	}
+	public String getSaleItem() {
+		return saleItem;
+	}
+	public void setSaleItem(String saleItem) {
+		this.saleItem = saleItem;
+	}
+	public String getSaleQty() {
+		return saleQty;
+	}
+	public void setSaleQty(String saleQty) {
+		this.saleQty = saleQty;
+	}
+}
--- ydERP/src/main/java/com/yedam/erp/view/SalesController.java
+++ ydERP/src/main/java/com/yedam/erp/view/SalesController.java
@@ -2,15 +2,38 @@ package com.yedam.erp.view;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.yedam.erp.sales.SalesService;
+import com.yedam.erp.sales.SalesVO;
 
 @Controller
-public class SalesController{
+public class SalesController {
 
 	@Autowired
 	SalesService salesService;
+
+	/* 전체 조회 폼 */
+	@RequestMapping("/getSaleList")
+	public String getSaleList(Model model, SalesVO vo) {
+		model.addAttribute("saleList", salesService.getSaleList(vo));
+		return "sales/getSaleList";
+	}
 	
+	/* 등록 폼 */
+	@RequestMapping("/insertSaleForm")
+	public String insertSaleForm() {
+		return "sales/insertSale";
+	}
+
+	/* 등록 처리 */
+	@RequestMapping("/insertSale")
+	public String insertSale(SalesVO vo) {
+		salesService.insertSale(vo);
+		return "redirect:/getSaleList";
+	}
 	
-	
+	/* 수정 처리 */
+
 }

48b45906c2a95cca61891e20e1d28dfcc0d21fa0
User <User@YD02-05>
2018-04-04 12:25:32 +0900

    ItemController added

---
+++ ydERP/src/main/java/com/yedam/erp/view/ItemsController.java
@@ -0,0 +1,101 @@
+package com.yedam.erp.view;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.yedam.erp.items.ItemsService;
+import com.yedam.erp.items.ItemsVO;
+
+@Controller
+public class ItemsController {
+
+	@Autowired
+	ItemsService itemsService;
+
+	// 등록폼
+	@RequestMapping("/insertItemsForm")
+	public String insertItemsForm() {
+		return "items/insertItems";
+	}
+
+	// 등록처리
+	@RequestMapping("/insertItems")
+	public String insertItems(ItemsVO vo) {
+		itemsService.insertItems(vo);
+		return "redirect:/getItemsList";
+	}
+
+	@RequestMapping("/getItemsList")
+	public String getItemsList(Model model, ItemsVO vo) {
+		model.addAttribute("ItemsList", itemsService.getItemsList(vo));
+		return "Items/getItemsList";
+	}
+
+	// 상세보기
+	@RequestMapping("/getItems")
+	public String getItems(Model model, ItemsVO vo) {
+		model.addAttribute("Items", itemsService.getItems(vo));
+		return "Items/getItems";
+
+	}
+
+	// 수정폼
+	@RequestMapping("/updateItemsForm")
+	public String updateItemsForm(Model model, ItemsVO vo) {
+
+		model.addAttribute("Items", itemsService.getItems(vo));
+		return "Items/updateItems";
+
+	}
+
+	// 수정처리
+	@RequestMapping("/updateItems")
+	public String updateItems(ItemsVO vo) {
+		itemsService.updateItems(vo);
+		return "redirect:/getItemsList";
+
+	}
+
+	// 삭제처리
+	// 삭제처리
+	@RequestMapping("/deleteItems")
+	public String deleteItems(ItemsVO vo) {
+		itemsService.deleteItems(vo);
+		return "redirect:/getItemsList";
+
+	}
+	
+
+	// 중복체크
+	@RequestMapping("/dupCheck")
+	@ResponseBody
+	public Map<String, Boolean> dupCheck(ItemsVO vo) {
+		HashMap<String, Boolean> map = new HashMap<String, Boolean>();
+		if (itemsService.getItems(vo) == null) {
+			map.put("result", true);
+		} else {
+			map.put("result", false);
+		}
+
+		return map;
+	}
+
+	// 로그 아웃
+
+	@RequestMapping("/logout")
+	public String logout(HttpSession session) {
+		session.invalidate();
+		return "redirect:getBoardList";
+	}
+
+}

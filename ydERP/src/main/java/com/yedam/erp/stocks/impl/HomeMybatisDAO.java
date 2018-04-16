72b50ed02b4a15b3fd4df4d10490c630ebb48df3
leadon <leadon@naver.com>
2018-04-16 15:46:37 +0900

    lookup

--- ydERP/src/main/java/com/yedam/erp/stocks/impl/HomeMybatisDAO.java
+++ ydERP/src/main/java/com/yedam/erp/stocks/impl/HomeMybatisDAO.java
@@ -17,6 +17,14 @@ public class HomeMybatisDAO {
 	}
 
 	public List<Map<String, Object>> getLookups() {
-		return sqlSession.selectList("stocks.getLookups");
+		return sqlSession.selectList("stocks.getLookupsData");
+	}
+
+	public List<Map<String, String>> getLookValues() {
+		return sqlSession.selectList("stocks.getLookupValues");
+	}
+
+	public List<Map<String, String>> selectLookups(String str) {
+		return sqlSession.selectList("stocks.selectLookups", str);
 	}
 }
